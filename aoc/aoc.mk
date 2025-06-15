b9d6d3841f36d99a6e878bf0f0607b1a4ea1c14d
Fred Underwood <underwoodfred@proton.me>
2025-06-15 12:30:42 +1000

    strip aoc

--- aoc/aoc.mk
+++ aoc/aoc.mk
@@ -1,25 +1,4 @@
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/aoc/sepolicy
 
-PRODUCT_PACKAGES += dump_aoc \
-		    aocd \
-		    aocxd
-
-# If AoC Daemon is not present on this build, load firmware at boot via rc
-ifeq ($(wildcard vendor/google/whitechapel/aoc/aocd),)
-PRODUCT_COPY_FILES += \
-	device/google/gs-common/aoc/conf/init.aoc.nodaemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
-else
-PRODUCT_COPY_FILES += \
-	device/google/gs-common/aoc/conf/init.aoc.daemon.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.aoc.rc
-endif
-
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
 	device/google/gs-common/proprietary/aocx.IAocx.xml
-
-# AoC debug support
-PRODUCT_PACKAGES_DEBUG += \
-	aocdump \
-	aocutil \
-	aoc_audio_cfg \
-	vp_util \
-	aocx_tool
