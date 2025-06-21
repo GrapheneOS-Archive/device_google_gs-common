55966c479a87ac7a500bfc663822ed0ffafd6022
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-06-21 16:32:02 +0000

    remove references to pixel_ril manifest from device repo, add TODO

--- pixel_ril/ril.mk
+++ pixel_ril/ril.mk
@@ -1,10 +1,12 @@
 BOARD_SEPOLICY_DIRS += device/google/gs-common/pixel_ril/sepolicy
 
-ifeq ($(SIM_COUNT), 2)
-    DEVICE_MANIFEST_FILE += device/google/gs-common/pixel_ril/manifest_ril_ds.xml
-else
-    DEVICE_MANIFEST_FILE += device/google/gs-common/pixel_ril/manifest_ril.xml
-endif
+# TODO: Verify that this is the correct approach for pixel_ril manifest extraction
+#       with corresponding compatibility matrix file declared here.
+# ifeq ($(SIM_COUNT), 2)
+#     DEVICE_MANIFEST_FILE += device/google/gs-common/pixel_ril/manifest_ril_ds.xml
+# else
+#     DEVICE_MANIFEST_FILE += device/google/gs-common/pixel_ril/manifest_ril.xml
+# endif
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs-common/pixel_ril/compatibility_matrix.xml
 
 USE_GOOGLE_RIL_EXT := true
